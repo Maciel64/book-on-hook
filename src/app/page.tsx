319b67c62d9f645da9482363291142459fcc9241
Maciel Suassuna <macielsuassuna14@gmail.com>
2025-03-13 22:35:25 -0300

    reset project

--- src/app/page.tsx
+++
@@ -1,9 +0,0 @@
-import DropFileInput from "@/components/drop-file-input";
-
-export default function Home() {
-  return (
-    <div className="pl-52 pr-48 pt-48">
-      <DropFileInput id="book-input" name="book-input" />
-    </div>
-  );
-}
